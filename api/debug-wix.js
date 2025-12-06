424ebbba8e6d7c35f9373e4a3ef65a49011235f7
baleanza <andrii.burzi@baleanza.com>
2025-12-06 04:08:33 +0200

    Change filter key from 'sku' to 'product.variantSku'

--- api/debug-wix.js
+++ api/debug-wix.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       body: JSON.stringify({
         query: {
           filter: { 
-            "sku": { "$in": [sku] } 
+            "product.variantSku": { "$in": [sku] } 
           }
         }
       })
